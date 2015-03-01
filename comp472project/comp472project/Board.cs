7a644d3cecce5e8fdfa153601f6c3205ee98c614
celestrial <sean.celestrial@gmail.com>
2015-03-01 00:04:53 -0500

    fixed messed up changes from source tree, good to go

--- comp472project/comp472project/Board.cs
+++ comp472project/comp472project/Board.cs
@@ -1,75 +1,80 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 
+namespace comp472project
+{
+    public class Board
+    {
+        const int SIZE = 8;
+        char[,] gameBoard;
 
-public class Board {
-	const int SIZE = 8;
-    char[,] gameBoard;
 
-
-	public void setBoard()
-	{
-		//gameBoard = new char[(int) System.Math.Pow(SIZE,2)];
-        gameBoard = new char[SIZE,SIZE];
-		makeBoard();
-	}
-
-	public void makeBoard()
-	{
-        for (int j = 0; j < SIZE; ++j)
+        public void setBoard()
         {
-            for (int i = 0; i < SIZE; ++i)
-            {
-                gameBoard[j,i] = 'E';
+            //gameBoard = new char[(int) System.Math.Pow(SIZE,2)];
+            gameBoard = new char[SIZE, SIZE];
+            makeBoard();
+        }
 
-                //if (j == SIZE - 1 || i == SIZE -1)
-                //    gameBoard[j, i] = 'E';
-                //else
-                //    gameBoard[j, i] = 'B';
+        public void makeBoard()
+        {
+            for (int j = 0; j < SIZE; ++j)
+            {
+                for (int i = 0; i < SIZE; ++i)
+                {
+                    gameBoard[j, i] = 'E';
+
+                    //if (j == SIZE - 1 || i == SIZE -1)
+                    //    gameBoard[j, i] = 'E';
+                    //else
+                    //    gameBoard[j, i] = 'B';
+                }
             }
         }
-	}
 
-	public Board()
-	{
-		setBoard ();
-	}
-
-	public void changeTile(char color, char row, int col)
-	{
-
-		int iRow = ((int) char.ToUpper(row)) - 65;
-		if(iRow >= SIZE || iRow < 0)
-			return;
-		if(char.ToUpper(color) == 'B')
-		{
-            gameBoard[iRow, col] = 'B';
-		}else{
-            gameBoard[iRow, col] = 'W';
-        }
-	}
-
-    public void changeTile(char color, int row, int col)
-    {
-        if (row >= SIZE || row < 0)
-            return;
-        if (char.ToUpper(color) == 'B')
+        public Board()
         {
-            gameBoard[row, col] = 'B';
+            setBoard();
         }
-        else
+
+        public void changeTile(char color, char row, int col)
         {
-            gameBoard[row, col] = 'W';
+
+            int iRow = ((int)char.ToUpper(row)) - 65;
+            if (iRow >= SIZE || iRow < 0)
+                return;
+            if (char.ToUpper(color) == 'B')
+            {
+                gameBoard[iRow, col] = 'B';
+            }
+            else
+            {
+                gameBoard[iRow, col] = 'W';
+            }
         }
-    }
 
-    public char getCell(int x, int y)
-    {
-        return gameBoard[x, y];
-    }
+        public void changeTile(char color, int row, int col)
+        {
+            if (row >= SIZE || row < 0)
+                return;
+            if (char.ToUpper(color) == 'B')
+            {
+                gameBoard[row, col] = 'B';
+            }
+            else
+            {
+                gameBoard[row, col] = 'W';
+            }
+        }
 
-    public int getSize()
-    {
-        return SIZE;
+        public char getCell(int x, int y)
+        {
+            return gameBoard[x, y];
+        }
+
+        public int getSize()
+        {
+            return SIZE;
+        }
     }
 }
